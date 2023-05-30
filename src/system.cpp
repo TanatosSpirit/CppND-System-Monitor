c71b27f43cc7b87ae08e70344ce85c1344ea6606
Andrei <lytovandrei@gmail.com>
2023-05-30 12:51:40 +0300

    Added OS and Kernel output

--- src/system.cpp
+++ src/system.cpp
@@ -16,6 +16,11 @@ using std::vector;
 
 You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
 
+System::System() {
+  os_ = LinuxParser::OperatingSystem();
+  kernel_ = LinuxParser::Kernel();
+}
+
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
@@ -23,13 +28,13 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return os_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
